57c299344e03412f823da65c0dd5ba217d376ff3
SamuelWade <samuelrcwade@gmail.com>
2023-07-01 10:35:13 -0400

    lab completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+
+# import ipdb
+# ipdb.set_trace()
 def write_file(file_name, file_content):
-    pass
+    print(f'--------------------i am here')
+    print(f'{file_name} I am here inside of here now')
+    with open(f'{file_name}.txt',mode='w',encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt',mode='a') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    text_file = open(f'{file_name}.txt', encoding='utf-8')
+    return text_file.read()
